a82967977fc7b88d44b48e18f6c3101d7ecf02ba
qianfeng <qianfeng@qianfengdeMac-mini-14.local>
2015-11-23 17:14:39 +0800

    l1

---
+++ LYNProject/LYNProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYNProject
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 LinTengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(void)createDemo;
+@end
+
---
+++ LYNProject/LYNProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LYNProject
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 LinTengfei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+-(void)createDemo{
+    NSLog(@"Demo");
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
